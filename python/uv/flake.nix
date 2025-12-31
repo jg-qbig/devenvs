9ec689e4ea3868f34653ae4c60eedb5cd8673182
wolfingten <jagu00005@stud.uni-saarland.de>
2025-12-31 12:07:03 +0100

    add imperative python uv env

---
+++ python/uv/flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in {
+    devShells = forAllSystems (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            uv
+            python312
+          ];
+        };
+      }
+    );
+  };
+}
